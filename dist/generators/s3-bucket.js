a5fee89fa1c65845a29b1eab554ee6acdb6bcaf3
Herczeg Máté <herczeg6179@gmail.com>
2019-03-24 20:36:52 +0100

    adding slightly refactored version of s3 bucket reader

--- dist/generators/s3-bucket.js
+++ dist/generators/s3-bucket.js
@@ -1,79 +1,120 @@
-"use strict";
+'use strict';
 
-// TODO: kind of a mess, maybe try a different approach with the listObjectsV2 result handling
-// probably should instead
-// map to add playlists names
-// reduce a playlist list from all
-// add tracks to playlists by filtering
-/*
-const AWS = require('aws-sdk/global');
-const { formatName } = require('../helpers');
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-const MaxKeys = 1000; // TODO: handle pages
+var AWS = require('aws-sdk/global');
+var S3 = require('aws-sdk/clients/s3');
 
-const isTrack = ({ Key }, Prefix) => Key.match(new RegExp(`${Prefix}\\/.*\\/.+`));
+var _require = require('../helpers'),
+    getTrackInfo = _require.getTrackInfo;
 
-const s3Factory = config => {
-    AWS.config.update(config);
-    return new AWS.S3({ ...config, apiVersion: '2006-03-01' });
+var MaxKeys = 1000; // TODO: handle pages
+
+var isTrack = function isTrack(_ref, folder) {
+    var Key = _ref.Key,
+        Size = _ref.Size;
+    return Size > 0 && Key.match(new RegExp(folder + '\\/.*\\/.+'));
 };
 
-const objectListToPlaylists = ({ s3objectlist: { Contents, Prefix }, url }) =>
-    Contents.reduce(
-        (playlists, s3object) =>
-            isTrack(s3object, Prefix)
-                ? appendToPlaylists({ Prefix, prevPlaylists: playlists, s3object, url })
-                : playlists,
-        []
-    );
+var getPlaylistName = function getPlaylistName(_ref2, folder) {
+    var Key = _ref2.Key;
+    return Key.replace(folder + '/', '').split('/').shift();
+};
 
-const findPlaylist = (playlistToFind, playlists) => playlists.find(({ playlist }) => playlist === playlistToFind);
+var getBucketUrl = function getBucketUrl(_ref3) {
+    var Bucket = _ref3.Bucket,
+        url = _ref3.url;
+    return url + '/' + Bucket;
+};
 
-const findPlaylistIndex = (playlistToFind, playlists) =>
-    playlists.findIndex(({ playlist }) => playlist === playlistToFind);
+var getFilename = function getFilename(_ref4, folder) {
+    var Key = _ref4.Key,
+        playlist = _ref4.playlist;
+    return Key.replace(folder + '/' + playlist, '');
+};
 
-const getTrackInfo = ({ url, filename }) => {
-    const formattedName = formatName(filename);
-    const extension = formattedName.includes('.') ? formattedName.split('.').pop() : '';
+var getFileUrl = function getFileUrl(_ref5, bucketUrl) {
+    var Key = _ref5.Key;
+    return bucketUrl + '/' + Key;
+};
 
+var s3Factory = function s3Factory(config) {
+    AWS.config.update(config);
+    return new S3(_extends({}, config, { apiVersion: '2006-03-01' }));
+};
+
+var uniquePlaylistNameReducer = function uniquePlaylistNameReducer(uniquePlaylists, _ref6) {
+    var playlist = _ref6.playlist;
+    return uniquePlaylists.includes(playlist) ? uniquePlaylists : uniquePlaylists.concat(playlist);
+};
+
+var initializePlaylist = function initializePlaylist(playlistName, tracklist) {
     return {
-        url,
-        name: formattedName.replace(`.${extension}`, ''),
-        extension,
+        playlist: playlistName,
+        tracks: tracklist.filter(function (track) {
+            return track.playlist === playlistName;
+        }).map(function (track) {
+            delete track.playlist; // can get rid fo it now, though would love a better way for this...
+            return _extends({}, track);
+        })
     };
 };
 
-const initializePlaylist = (playlistToInit, playlists) =>
-    !findPlaylist(playlistToInit, playlists) ? [...playlists, { playlist: playlistToInit, tracks: [] }] : playlists;
-
-const appendToPlaylists = ({ Prefix, prevPlaylists, s3object: { Key }, url }) => {
-    const keyParts = Key.replace(`${Prefix}/`, '').split('/');
-    const playlist = keyParts.shift();
-    const filename = keyParts.join('/');
-
-    const nextPlaylists = initializePlaylist(playlist, prevPlaylists);
-    const playlistIndex = findPlaylistIndex(playlist, nextPlaylists);
-
-    nextPlaylists[playlistIndex].tracks = appendTrack(
-        getTrackInfo({ url: `${url}${Key}`, filename }),
-        nextPlaylists[playlistIndex].tracks
-    );
-    return nextPlaylists;
+var getS3FolderContent = function getS3FolderContent(_ref7) {
+    var S3Config = _ref7.S3Config,
+        Bucket = _ref7.Bucket,
+        Prefix = _ref7.Prefix;
+    return new Promise(function (resolve, reject) {
+        s3Factory(S3Config).listObjectsV2({ Bucket: Bucket, MaxKeys: MaxKeys, Prefix: Prefix }, function (error, s3Response) {
+            return error ? reject(error) : resolve(s3Response);
+        });
+    });
 };
 
-const appendTrack = (track, tracks) => [...tracks, track];
-
-const s3BucketToPLaylists = ({ S3Config, Bucket, folder, url }) => {
-    const s3 = s3Factory(S3Config);
-
-    return new Promise((resolve, reject) => {
-        s3.listObjectsV2({ Bucket, MaxKeys, Prefix: folder }, (error, s3Response) =>
-            error ? reject(error) : resolve(s3Response)
-        );
-    }).then(s3objectlist => objectListToPlaylists({ s3objectlist, url }));
+var filterS3ResultToTracks = function filterS3ResultToTracks(_ref8) {
+    var folder = _ref8.folder;
+    return function (s3ObjectList) {
+        return s3ObjectList.Contents.filter(function (s3Object) {
+            return isTrack(s3Object, folder);
+        });
+    };
 };
 
-module.exports = { s3BucketToPLaylists };
-*/
+var transformS3ObjectsToTracks = function transformS3ObjectsToTracks(_ref9) {
+    var folder = _ref9.folder,
+        bucketUrl = _ref9.bucketUrl;
+    return function (s3ObjectList) {
+        return s3ObjectList.map(function (s3Object) {
+            return _extends({}, getTrackInfo({ filename: getFilename(s3Object, folder), url: getFileUrl(s3Object, bucketUrl) }), {
+                playlist: s3Object.playlist // we need to keep this around for the reduce
+            });
+        });
+    };
+};
 
-module.exports = {};
+var addPlaylistNames = function addPlaylistNames(_ref10) {
+    var folder = _ref10.folder;
+    return function (s3ObjectList) {
+        return s3ObjectList.map(function (s3Object) {
+            return _extends({}, s3Object, {
+                playlist: getPlaylistName(s3Object, folder)
+            });
+        });
+    };
+};
+
+var createPlaylists = function createPlaylists() {
+    return function (tracklist) {
+        return tracklist.reduce(uniquePlaylistNameReducer, []).map(function (playlistName) {
+            return initializePlaylist(playlistName, tracklist);
+        });
+    };
+};
+
+module.exports = function (_ref11) {
+    var S3Config = _ref11.S3Config,
+        Bucket = _ref11.Bucket,
+        folder = _ref11.folder,
+        url = _ref11.url;
+    return getS3FolderContent({ S3Config: S3Config, Bucket: Bucket, Prefix: folder }).then(filterS3ResultToTracks({ folder: folder })).then(addPlaylistNames({ folder: folder })).then(transformS3ObjectsToTracks({ folder: folder, bucketUrl: getBucketUrl({ Bucket: Bucket, url: url }) })).then(createPlaylists());
+};
--- src/generators/s3-bucket.js
+++ src/generators/s3-bucket.js
@@ -1,77 +1,68 @@
-// TODO: kind of a mess, maybe try a different approach with the listObjectsV2 result handling
-// probably should instead
-// map to add playlists names
-// reduce a playlist list from all
-// add tracks to playlists by filtering
-/*
 const AWS = require('aws-sdk/global');
-const { formatName } = require('../helpers');
+const S3 = require('aws-sdk/clients/s3');
+const { getTrackInfo } = require('../helpers');
 
 const MaxKeys = 1000; // TODO: handle pages
 
-const isTrack = ({ Key }, Prefix) => Key.match(new RegExp(`${Prefix}\\/.*\\/.+`));
+const isTrack = ({ Key, Size }, folder) => Size > 0 && Key.match(new RegExp(`${folder}\\/.*\\/.+`));
+
+const getPlaylistName = ({ Key }, folder) =>
+    Key.replace(`${folder}/`, '')
+        .split('/')
+        .shift();
+
+const getBucketUrl = ({ Bucket, url }) => `${url}/${Bucket}`;
+
+const getFilename = ({ Key, playlist }, folder) => Key.replace(`${folder}/${playlist}`, '');
+
+const getFileUrl = ({ Key }, bucketUrl) => `${bucketUrl}/${Key}`;
 
 const s3Factory = config => {
     AWS.config.update(config);
-    return new AWS.S3({ ...config, apiVersion: '2006-03-01' });
+    return new S3({ ...config, apiVersion: '2006-03-01' });
 };
 
-const objectListToPlaylists = ({ s3objectlist: { Contents, Prefix }, url }) =>
-    Contents.reduce(
-        (playlists, s3object) =>
-            isTrack(s3object, Prefix)
-                ? appendToPlaylists({ Prefix, prevPlaylists: playlists, s3object, url })
-                : playlists,
-        []
-    );
+const uniquePlaylistNameReducer = (uniquePlaylists, { playlist }) =>
+    uniquePlaylists.includes(playlist) ? uniquePlaylists : uniquePlaylists.concat(playlist);
 
-const findPlaylist = (playlistToFind, playlists) => playlists.find(({ playlist }) => playlist === playlistToFind);
+const initializePlaylist = (playlistName, tracklist) => ({
+    playlist: playlistName,
+    tracks: tracklist
+        .filter(track => track.playlist === playlistName)
+        .map(track => {
+            delete track.playlist; // can get rid fo it now, though would love a better way for this...
+            return { ...track };
+        }),
+});
 
-const findPlaylistIndex = (playlistToFind, playlists) =>
-    playlists.findIndex(({ playlist }) => playlist === playlistToFind);
-
-const getTrackInfo = ({ url, filename }) => {
-    const formattedName = formatName(filename);
-    const extension = formattedName.includes('.') ? formattedName.split('.').pop() : '';
-
-    return {
-        url,
-        name: formattedName.replace(`.${extension}`, ''),
-        extension,
-    };
-};
-
-const initializePlaylist = (playlistToInit, playlists) =>
-    !findPlaylist(playlistToInit, playlists) ? [...playlists, { playlist: playlistToInit, tracks: [] }] : playlists;
-
-const appendToPlaylists = ({ Prefix, prevPlaylists, s3object: { Key }, url }) => {
-    const keyParts = Key.replace(`${Prefix}/`, '').split('/');
-    const playlist = keyParts.shift();
-    const filename = keyParts.join('/');
-
-    const nextPlaylists = initializePlaylist(playlist, prevPlaylists);
-    const playlistIndex = findPlaylistIndex(playlist, nextPlaylists);
-
-    nextPlaylists[playlistIndex].tracks = appendTrack(
-        getTrackInfo({ url: `${url}${Key}`, filename }),
-        nextPlaylists[playlistIndex].tracks
-    );
-    return nextPlaylists;
-};
-
-const appendTrack = (track, tracks) => [...tracks, track];
-
-const s3BucketToPLaylists = ({ S3Config, Bucket, folder, url }) => {
-    const s3 = s3Factory(S3Config);
-
-    return new Promise((resolve, reject) => {
-        s3.listObjectsV2({ Bucket, MaxKeys, Prefix: folder }, (error, s3Response) =>
+const getS3FolderContent = ({ S3Config, Bucket, Prefix }) =>
+    new Promise((resolve, reject) => {
+        s3Factory(S3Config).listObjectsV2({ Bucket, MaxKeys, Prefix }, (error, s3Response) =>
             error ? reject(error) : resolve(s3Response)
         );
-    }).then(s3objectlist => objectListToPlaylists({ s3objectlist, url }));
-};
+    });
 
-module.exports = { s3BucketToPLaylists };
-*/
+const filterS3ResultToTracks = ({ folder }) => s3ObjectList =>
+    s3ObjectList.Contents.filter(s3Object => isTrack(s3Object, folder));
 
-module.exports = {};
+const transformS3ObjectsToTracks = ({ folder, bucketUrl }) => s3ObjectList =>
+    s3ObjectList.map(s3Object => ({
+        ...getTrackInfo({ filename: getFilename(s3Object, folder), url: getFileUrl(s3Object, bucketUrl) }),
+        playlist: s3Object.playlist, // we need to keep this around for the reduce
+    }));
+
+const addPlaylistNames = ({ folder }) => s3ObjectList =>
+    s3ObjectList.map(s3Object => ({
+        ...s3Object,
+        playlist: getPlaylistName(s3Object, folder),
+    }));
+
+const createPlaylists = () => tracklist =>
+    tracklist.reduce(uniquePlaylistNameReducer, []).map(playlistName => initializePlaylist(playlistName, tracklist));
+
+module.exports = ({ S3Config, Bucket, folder, url }) =>
+    getS3FolderContent({ S3Config, Bucket, Prefix: folder })
+        .then(filterS3ResultToTracks({ folder }))
+        .then(addPlaylistNames({ folder }))
+        .then(transformS3ObjectsToTracks({ folder, bucketUrl: getBucketUrl({ Bucket, url }) }))
+        .then(createPlaylists());
